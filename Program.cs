9b953385c211b8ad9fbd2c27b7779ec4428ab8d5
safacanmetin <safacanmetin@gmail.com>
2024-06-08 23:09:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the operator (+, -, *, /): ");
+        string operation = Console.ReadLine();
+
+        Console.WriteLine("Enter the second number:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double result;
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) 
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Invalid operator.");
+                return;
+        }
+
+        Console.WriteLine("The result is: " + result);
+    }
+}
